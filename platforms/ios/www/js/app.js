d97c1b089f78434b4bd55bb4e5a629e2d5cf2399
harv0116 <harv0116@algonquinlive.com>
2015-03-31 16:04:30 -0400

    updates

--- platforms/ios/www/js/app.js
+++ platforms/ios/www/js/app.js
@@ -1,6 +1,8 @@
 //app.js
 
 var app= {
+    db:null,
+	version:'1.0',
 	loadRequirements:0,
 	init: function(){
 		document.addEventListener("deviceready", app.onDeviceReady);
@@ -20,38 +22,124 @@ var app= {
 	},
 	start: function(){
 		app.opendb();
+		app.createdb();
 		app.fillpeople();
-		app.filloccasions();
+		//app.filloccasions();
 	
 		//connect to database
 		//build the lists for the main pages based on data
 		//add button and navigation listeners
 	},
 	opendb: function(){
-		db = opendatabase('giftr','','Giftr',1024*1024);
+		app.db = opendatabase('giftr','','Giftr',1024*1024);
 	},
-	fillpeople: function(){
-		trans.executeSql("SELECT person_name FROM people", [ ], app.peopleSuccess, app.transErr);
-	
-	},
-	filloccasions: function(){
-		trans.executeSql("SELECT occ_name FROM occasions", [ ], app.occSuccess, app.transErr);
-
-	},
-	peopleSuccess: function( trans, results){
-		var len = results.rows.length;
-		var ul = document.querySelectorAll("[data-role=listview]")[0];
-		for (var i=0; i<len; i++) {
-			// display one person_name 
-			var li = document.createElement("li");
-			li.dataset.ref = i;
-			li.innerHTML = results.rows.item(i);
-			ul.appendChild(li);
+	createdb: function(){
+		if(app.version == ''){
+			output('First time running... create tables'); 
+			//means first time creation of DB
+			//increment the version and create the tables
+			app.db.changeVersion('', app.version,
+					function(trans){
+						//something to do in addition to incrementing the value
+						//otherwise your new version will be an empty DB
+						output("DB version incremented");
+						//do the initial setup
+								//create some table(s)
+								//add stuff into table(s)
+						trans.executeSql('CREATE TABLE people(person_id INTEGER PRIMARY KEY AUTOINCREMENT, person_name TEXT)', [], 
+										function(tx, rs){
+											//do something if it works
+											output("Table people created");
+										},
+										function(tx, err){
+											//failed to run query
+											output( err.message);
+										});
+										
+						trans.executeSql('INSERT INTO people(person_name) VALUES(?)', ["Paul"], 
+										function(tx, rs){
+											//do something if it works, as desired   
+											output("Added row in people");
+										},
+										function(tx, err){
+											//failed to run query
+											output( err.message);
+										});
+						trans.executeSql('CREATE TABLE occasions(occ_id INTEGER PRIMARY KEY AUTOINCREMENT, occ_name TEXT)', [], 
+										function(tx, rs){
+											//do something if it works
+											output("Table occ created");
+										},
+										function(tx, err){
+											//failed to run query
+											output( err.message);
+										});
+										
+						trans.executeSql('INSERT INTO occasions(occ_name) VALUES(?)', ["Christmas"], 
+										function(tx, rs){
+											//do something if it works, as desired   
+											output("Added row in occ");
+										},
+										function(tx, err){
+											//failed to run query
+											output( err.message);
+										});
+					},
+					function(err){
+						//error in changing version
+						//if the increment fails
+						output( "Change version call error " + err.message);
+					},
+					function(){
+						//successfully completed the transaction of incrementing the version number   
+								output("Change version function worked.")
+					});
+		}else{
+			//version should be 1.0
+			//this won't be the first time running the app
+			output("DB has previously been created");
+			output('Version:' + app.version)   ;
 		}
 	},
+	fillpeople: function(){
+        document.querySelector("#people-list").style.display="block";
+        document.querySelector("#occasion-list").style.display="none";
+        document.querySelector("#gifts-for-person").style.display="none";
+        document.querySelector("#gifts-for-occasion").style.display="none";
+        
+        var ul = document.querySelectorAll("[data-role=listview]")[0];
+        ul.innerHTML = "";
+        
+        app.db.transaction(function(trans){
+        trans.executeSql("SELECT person_name FROM people", [],
+                function(tx, rs){
+                         var len = results.rows.length;
+
+                         for (var i=0; i<len; i++) {
+                         // display one person_name
+							 var li = document.createElement("li");
+							 li.dataset.ref = i;
+							 li.innerHTML = results.rows.item(i);
+							 ul.appendChild(li);
+                         }
+                },
+                function(tx, err){
+                    console.log("Error: " + err);
+                });
+            });
+	},
+	filloccasions: function(){
+        document.querySelector("#people-list").style.display="none";
+        document.querySelector("#occasion-list").style.display="block";
+        document.querySelector("#gifts-for-person").style.display="none";
+        document.querySelector("#gifts-for-occasion").style.display="none";
+        
+        //trans.executeSql("SELECT occ_name FROM occasions", [], app.occSuccess, app.transErr);
+
+	},
 	occSuccess: function (trans, results){
-		var len2 = results.rows.length;
-		var ul2 = document.querySelectorAll("[data-role=listview]")[1];
+	/*	var len2 = results.rows.length;
+		var ul2 = document.querySelectorAll(["data-role=listview"])[1];
 		for (var i=0; i<len2; i++) {
 			// display one occ_name 
 			var li2 = document.createElement("li");
@@ -59,7 +147,7 @@ var app= {
 			li2.innerHTML = results.rows.item(i);
 			ul2.appendChild(li2);
 		}	
-	
+	*/
 	},
 	transErr: function (err) {
 		console.info (err.message);
--- www/js/app.js
+++ www/js/app.js
@@ -1,70 +1,215 @@
 //app.js
 
 var app= {
+    db:null,
+	version:'1.0',
 	loadRequirements:0,
 	init: function(){
-		document.addEventListener("deviceready", app.onDeviceReady);
+		//document.addEventListener("deviceready", app.onDeviceReady);
 		document.addEventListener("DOMContentLoaded", app.onDomReady);
 	},
 	onDeviceReady: function(){
-		app.loadRequirements++;
-		if(app.loadRequirements === 2){
-			app.start();
-		}
+		//app.loadRequirements++;
+		//if(app.loadRequirements === 2){
+			//app.start();
+		//}
 	},
 	onDomReady: function(){
-		app.loadRequirements++;
-		if(app.loadRequirements === 2){
+		//app.loadRequirements++;
+		//if(app.loadRequirements === 2){
 			app.start();
-		}
+		//}
 	},
 	start: function(){
-		app.opendb();
+		app.createdb();
 		app.fillpeople();
-		app.filloccasions();
+		//app.filloccasions();
 	
 		//connect to database
 		//build the lists for the main pages based on data
 		//add button and navigation listeners
 	},
-	opendb: function(){
-		db = opendatabase('giftr','','Giftr',1024*1024);
-	},
-	fillpeople: function(){
-		trans.executeSql("SELECT person_name FROM people", [ ], app.peopleSuccess, app.transErr);
-	
-	},
-	filloccasions: function(){
-		trans.executeSql("SELECT occ_name FROM occasions", [ ], app.occSuccess, app.transErr);
-
-	},
-	peopleSuccess: function( trans, results){
-		var len = results.rows.length;
-		var ul = document.querySelectorAll("[data-role=listview]")[0];
-		for (var i=0; i<len; i++) {
-			// display one person_name 
-			var li = document.createElement("li");
-			li.dataset.ref = i;
-			li.innerHTML = results.rows.item(i);
-			ul.appendChild(li);
+	createdb: function(){
+		app.db = openDatabase('giftr','','Giftr',1024*1024);
+		
+		if(app.version == ''){
+			console.log('First time running... create tables'); 
+			//means first time creation of DB
+			//increment the version and create the tables
+			app.db.changeVersion('', app.version,
+					function(trans){
+						//something to do in addition to incrementing the value
+						//otherwise your new version will be an empty DB
+						console.log("DB version incremented");
+						//do the initial setup
+								//create some table(s)
+								//add stuff into table(s)
+						trans.executeSql('CREATE TABLE people(person_id INTEGER PRIMARY KEY AUTOINCREMENT, person_name TEXT)', [], 
+										function(tx, rs){
+											//do something if it works
+											console.log("Table people created");
+										},
+										function(tx, err){
+											//failed to run query
+											console.log( err.message);
+										});
+										
+						trans.executeSql('INSERT INTO people(person_name) VALUES(?)', ["Paul"], 
+										function(tx, rs){
+											//do something if it works, as desired   
+											console.log("Added row in people");
+										},
+										function(tx, err){
+											//failed to run query
+											console.log( err.message);
+										});
+						trans.executeSql('CREATE TABLE occasions(occ_id INTEGER PRIMARY KEY AUTOINCREMENT, occ_name TEXT)', [], 
+										function(tx, rs){
+											//do something if it works
+											console.log("Table occ created");
+										},
+										function(tx, err){
+											//failed to run query
+											console.log( err.message);
+										});
+										
+						trans.executeSql('INSERT INTO occasions(occ_name) VALUES(?)', ["Christmas"], 
+										function(tx, rs){
+											//do something if it works, as desired   
+											console.log("Added row in occ");
+										},
+										function(tx, err){
+											//failed to run query
+											console.log( err.message);
+										});
+						
+						trans.executeSql('CREATE TABLE gifts(gift_id INTEGER PRIMARY KEY AUTOINCREMENT, person_id INTEGER, occ_id INTEGER, gift_idea TEXT, purchased TEXT)', [], 
+										function(tx, rs){
+											//do something if it works
+											console.log("Table gift created");
+										},
+										function(tx, err){
+											//failed to run query
+											console.log( err.message);
+										});
+					},
+					function(err){
+						//error in changing version
+						//if the increment fails
+						console.log( "Change version call error " + err.message);
+					},
+					function(){
+						//successfully completed the transaction of incrementing the version number   
+								app.version = '1.0';
+								console.log("Change version function worked.");
+					});
+		}else{
+			//version should be 1.0
+			//this won't be the first time running the app
+			console.log("DB has previously been created");
+			console.log('Version:' + app.version)   ;
 		}
 	},
-	occSuccess: function (trans, results){
-		var len2 = results.rows.length;
-		var ul2 = document.querySelectorAll("[data-role=listview]")[1];
-		for (var i=0; i<len2; i++) {
-			// display one occ_name 
-			var li2 = document.createElement("li");
-			li2.dataset.ref = i;
-			li2.innerHTML = results.rows.item(i);
-			ul2.appendChild(li2);
-		}	
-	
-	},
-	transErr: function (err) {
-		console.info (err.message);
-	}
-	
-}
+	fillpeople: function(){
+        document.querySelector("#people-list").style.display="block";
+        document.querySelector("#occasion-list").style.display="none";
+        document.querySelector("#gifts-for-person").style.display="none";
+        document.querySelector("#gifts-for-occasion").style.display="none";
+        
+        var ul = document.querySelectorAll('[data-role=listview]')[0];
+        ul.innerHTML = "";
+        
+        app.db.transaction(function(trans){
+        trans.executeSql("SELECT person_name FROM people", [],
+                function(tx, rs){
+                         var len = rs.rows.length;
 
+                         for (var i=0; i<len; i++) {
+                         // display one person_name
+							 var li = document.createElement("li");
+							 li.dataset.ref = i;
+							 li.innerHTML = rs.rows.item(i).person_name;
+							 ul.appendChild(li);
+                         }
+                },
+                function(tx, err){
+                    console.log("Error: " + err);
+                });
+            });		
+		
+			var hammertime = new Hammer.Manager(ul);	
+			var swipeRight = new Hammer.Tap({event: 'swiperight' });
+			var singleTap = new Hammer.Tap({ event: 'singletap' });
+			var doubleTap = new Hammer.Tap({event: 'doubletap', taps: 2});
+			hammertime.add([doubleTap, singleTap, swipeRight]);
+			doubleTap.requireFailure('singletap');
+
+			hammertime.on('swiperight', function(ev) {
+				ev.preventDefault();
+				console.log(ev);
+				app.filloccasions(ev);
+			});
+
+			hammertime.on('singletap', function(ev) {
+				ev.preventDefault();
+				console.log(ev);
+				//app.view(ev);
+			});
+			hammertime.on('doubletap', function(ev) {
+				ev.preventDefault();
+				console.log(ev);
+				//app.map(ev);
+			});
+	},
+	filloccasions: function(){
+        document.querySelector("#people-list").style.display="none";
+        document.querySelector("#occasion-list").style.display="block";
+        document.querySelector("#gifts-for-person").style.display="none";
+        document.querySelector("#gifts-for-occasion").style.display="none";
+        
+        var ul = document.querySelectorAll('[data-role=listview]')[1];
+        ul.innerHTML = "";
+        
+        app.db.transaction(function(trans){
+        trans.executeSql("SELECT occ_name FROM occasions", [],
+                function(tx, rs){
+                         var len = rs.rows.length;
+
+                         for (var i=0; i<len; i++) {
+                         // display one person_name
+							 var li = document.createElement("li");
+							 li.dataset.ref = i;
+							 li.innerHTML = rs.rows.item(i).occ_name;
+							 ul.appendChild(li);
+                         }
+                },
+                function(tx, err){
+                    console.log("Error: " + err);
+                });
+        });
+		var hammertime = new Hammer.Manager(ul);	
+		var swipeLeft = new Hammer.Tap({event: 'swipeleft' });
+		var singleTap = new Hammer.Tap({ event: 'singletap' });
+		var doubleTap = new Hammer.Tap({event: 'doubletap', taps: 2});
+		hammertime.add([doubleTap, singleTap, swipeRight]);
+		doubleTap.requireFailure('singletap');
+
+		hammertime.on('swipeleft', function(ev) {
+			ev.preventDefault();
+			console.log(ev);
+			app.fillpeople(ev);
+		});
+
+		hammertime.on('singletap', function(ev) {
+			ev.preventDefault();
+			console.log(ev);
+			//app.view(ev);
+		});
+		hammertime.on('doubletap', function(ev) {
+			ev.preventDefault();
+			console.log(ev);
+			//app.map(ev);
+		});
+	}
+}
 app.init();
